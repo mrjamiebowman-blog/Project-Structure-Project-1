6b1a96adfd3620f7ba69f1816ce790f40e49fb27
Jamie Bowman <jamie.bowman@csiweb.com>
2020-12-15 08:24:27 -0600

    initial create

---
+++ src/MrJb.Project1.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MrJb.Project1.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ tests/MrJb.Project1.Data.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MrJb.Project1.Data.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
